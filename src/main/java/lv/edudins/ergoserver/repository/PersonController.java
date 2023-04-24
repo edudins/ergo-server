e8b1504d3ee6b8b1fde7157f83eb0401ced1b257
dudonz <edgars@dudins.lv>
2023-04-24 18:29:46 +0300

    Move service layer out of controller to service

--- src/main/java/lv/edudins/ergoserver/repository/PersonController.java
+++ src/main/java/lv/edudins/ergoserver/repository/PersonController.java
@@ -8,7 +8,6 @@ import org.springframework.hateoas.IanaLinkRelations;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -19,12 +18,10 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 @RestController
 class PersonController {
 
-    private final PersonRepository repository;
     private final PersonModelAssembler assembler;
     private final PersonService service;
 
-    PersonController(PersonRepository repository, PersonModelAssembler assembler, PersonService service) {
-        this.repository = repository;
+    PersonController(PersonModelAssembler assembler, PersonService service) {
         this.assembler = assembler;
         this.service = service;
     }
@@ -49,7 +46,7 @@ class PersonController {
 
     @GetMapping("/persons/{id}")
     EntityModel<Person> one(@PathVariable Long id) {
-        Person person = repository.findById(id)
+        Person person = service.findById(id)
                 .orElseThrow(() -> new PersonNotFoundException(id));
 
         return assembler.toModel(person);
@@ -60,21 +57,11 @@ class PersonController {
         String firstName = params.get("firstName");
         String lastName = params.get("lastName");
         String dateOfBirth = params.get("dateOfBirth");
-        List<EntityModel<Person>> persons;
 
-        if (dateOfBirth != null) {
-            LocalDate parsed = LocalDate.parse(dateOfBirth);
-            persons = repository.findByDateOfBirth(parsed)
-                    .stream()
-                    .map(assembler::toModel)
-                    .toList();
-        } else {
-            persons = repository.findByLastName(lastName)
-                    .stream()
-                    .filter(p -> p.getFirstName().equals(firstName))
-                    .map(assembler::toModel)
-                    .toList();
-        }
+        List<EntityModel<Person>> persons = service.search(firstName, lastName, dateOfBirth)
+                .stream()
+                .map(assembler::toModel)
+                .toList();
 
         if (persons.size() == 0) {
             throw new PersonNotFoundException(firstName + " " + lastName + " " + dateOfBirth);
@@ -85,26 +72,12 @@ class PersonController {
 
     @PutMapping("/persons/{id}")
     Person replacePerson(@RequestBody Person newPerson, @PathVariable Long id) {
-
-        return repository.findById(id)
-                .map(person -> {
-                    person.setFirstName(newPerson.getFirstName());
-                    person.setLastName(newPerson.getLastName());
-                    person.setGender(newPerson.getGender());
-                    person.setDateOfBirth(newPerson.getDateOfBirth());
-                    person.setPhoneNumber(newPerson.getPhoneNumber());
-                    person.setEmail(newPerson.getEmail());
-                    return repository.save(person);
-                })
-                .orElseGet(() -> {
-                    newPerson.setId(id);
-                    return repository.save(newPerson);
-                });
+        return service.replace(newPerson, id);
     }
 
     @DeleteMapping("/persons/{id}")
     ResponseEntity<?> deletePerson(@PathVariable Long id) {
-        repository.deleteById(id);
+        service.deleteById(id);
         return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/lv/edudins/ergoserver/service/PersonService.java
+++ src/main/java/lv/edudins/ergoserver/service/PersonService.java
@@ -4,7 +4,9 @@ import lv.edudins.ergoserver.domain.Person;
 import lv.edudins.ergoserver.repository.PersonRepository;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PersonService {
@@ -20,5 +22,42 @@ public class PersonService {
 
     public Person save(Person newPerson) {
         return repository.save(newPerson);
+    }
+
+    public Optional<Person> findById(Long id) {
+        return repository.findById(id);
+    }
+
+    public List<Person> search(String firstName, String lastName, String dateOfBirth) {
+        if (dateOfBirth != null) {
+            LocalDate parsed = LocalDate.parse(dateOfBirth);
+            return repository.findByDateOfBirth(parsed);
+        } else {
+            return repository.findByLastName(lastName)
+                    .stream()
+                    .filter(p -> p.getFirstName().equals(firstName))
+                    .toList();
+        }
+    }
+
+    public Person replace(Person newPerson, Long id) {
+        return repository.findById(id)
+                .map(person -> {
+                    person.setFirstName(newPerson.getFirstName());
+                    person.setLastName(newPerson.getLastName());
+                    person.setGender(newPerson.getGender());
+                    person.setDateOfBirth(newPerson.getDateOfBirth());
+                    person.setPhoneNumber(newPerson.getPhoneNumber());
+                    person.setEmail(newPerson.getEmail());
+                    return repository.save(person);
+                })
+                .orElseGet(() -> {
+                    newPerson.setId(id);
+                    return repository.save(newPerson);
+                });
+    }
+
+    public void deleteById(Long id) {
+        repository.deleteById(id);
     }
 }
